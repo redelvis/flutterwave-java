de005797dab49e9c1b350244b738aef14aa50310
Sergey Ponomarev <sponomarev@tain.com>
2016-09-21 19:33:02 +0300

    Fix missing GetPaidAccounts.hardner initialization

--- src/main/java/com/flutterwave/GetPaidAccounts.java
+++ src/main/java/com/flutterwave/GetPaidAccounts.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.flutterwave;
 
 import com.flutterwave.exceptions.EmptyKeyException;
@@ -39,6 +34,7 @@ public class GetPaidAccounts {
         this.apiKey = apiKey;
         this.merchantId = merchantId;
         this.baseUrl = baseUrl;
+        this.hardner = new RequestHardner();
     }
 
     public AccountResponse initiate(AccountRequest request) throws InvalidRequestObjectException, BadPaddingException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, IOException, URISyntaxException {
