761da13f9f000d243f945df2df8b9fa2c30092a9
Sergey Ponomarev <sponomarev@tain.com>
2016-09-27 17:14:49 +0300

    Add merchantid binding to RequestHardner

--- src/main/java/com/flutterwave/util/RequestHardner.java
+++ src/main/java/com/flutterwave/util/RequestHardner.java
@@ -14,27 +14,28 @@ public class RequestHardner {
     public MVVARequest MVVACardAPIHardner(MVVARequest mo) {
         MVVARequest mvo = new MVVARequest();
 
-        mvo.setCardno(tripleDes.harden(mo.getCardno()));
+        mvo.setMerchantid(mo.getMerchantid());
+        mvo.setAuthmodel(tripleDes.harden(mo.getAuthmodel()));
         mvo.setAmount(tripleDes.harden(mo.getAmount()));
-        mvo.setBvn(tripleDes.harden(mo.getBvn()));
-        mvo.setChargetoken(tripleDes.harden(mo.getChargetoken()));
         mvo.setCurrency(tripleDes.harden(mo.getCurrency()));
         mvo.setCurrency(tripleDes.harden(mo.getCurrency()));
-        mvo.setCustid(tripleDes.harden(mo.getCustid()));
-        mvo.setCvv(tripleDes.harden(mo.getCvv()));
+        mvo.setCountry(tripleDes.harden(mo.getCountry()));
+        mvo.setCardtype(tripleDes.harden(mo.getCardtype()));
+        mvo.setCardno(tripleDes.harden(mo.getCardno()));
         mvo.setExpirymonth(tripleDes.harden(mo.getExpirymonth()));
         mvo.setExpiryyear(tripleDes.harden(mo.getExpiryyear()));
+        mvo.setCustid(tripleDes.harden(mo.getCustid()));
         mvo.setNarration(tripleDes.harden(mo.getNarration()));
+        mvo.setChargetoken(tripleDes.harden(mo.getChargetoken()));
+        mvo.setCvv(tripleDes.harden(mo.getCvv()));
+        mvo.setBvn(tripleDes.harden(mo.getBvn()));
+        mvo.setPin(tripleDes.harden(mo.getPin()));
         mvo.setOtp(tripleDes.harden(mo.getOtp()));
         mvo.setOtptransactionidentifier(tripleDes.harden(mo.getOtptransactionidentifier()));
-        mvo.setPin(tripleDes.harden(mo.getPin()));
-        mvo.setAuthmodel(tripleDes.harden(mo.getAuthmodel()));
         mvo.setResponseurl(tripleDes.harden(mo.getResponseurl()));
         mvo.setValidateoption(tripleDes.harden(mo.getValidateoption()));
         mvo.setTrxauthorizeid(tripleDes.harden(mo.getTrxauthorizeid()));
         mvo.setTrxreference(tripleDes.harden(mo.getTrxreference()));
-        mvo.setCardtype(tripleDes.harden(mo.getCardtype()));
-        mvo.setCountry(tripleDes.harden(mo.getCountry()));
 
         return mvo;
     }
